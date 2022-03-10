e861c788efafb0aff918c39a7b732527f064f117
wyhallenwu <wyhforwork66@gmail.com>
2022-03-10 23:26:56 +0800

    add all process but need prune

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"todotxt/internal/file"
 )
 
 func main() {
 	file.Run()
+	file.CreateAndInsert("test insert")
+	file.Finished(1)
+	file.ShowFile("../todoList/Done.json")
+	fmt.Println("=======================")
+	file.ShowFile("../todoList/ToDo.json")
 }
